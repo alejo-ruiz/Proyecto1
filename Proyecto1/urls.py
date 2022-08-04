31fe2e673c1bc2949736b29e8c21cae3ce348e86
alejo ruiz <alejoeliasruiz041@gmail.com>
2022-08-03 22:02:17 -0500

    nuevos familiares

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from Proyecto1.views import despedida, saludo, template
-from blog.views import create_family
+from blog.views import create_family, list_family
 
 
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path("saludo/", saludo),
     path("despedida/", despedida),
     path("template/", template),
-    path("create_family/", create_family, name="create_family")
+    path("create_family/", create_family, name="create_family"),
+    path("list_family/", list_family, name="list_family")
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,26 @@ from django.shortcuts import render
 from blog.models import Family
 
 def create_family(request):
-    new_family = Family.objects.create(name = "Eduardo", years = 55)
+    new_family = Family.objects.create(name = "Eduardo", years = 55, last_name = "Simpson")
+    
+    new_family2 = Family.objects.create(name = "Laura", years = 56, last_name = "Simpson")
+    
+    new_family3 = Family.objects.create(name = "Emir", years = 21, last_name = "Simpson")
+    
     context = {
-        "new_family" :new_family
+        "new_family" :new_family,
+
+        "new_family2" :new_family2,
+
+        "new_family3" :new_family3
+
     }
     return render(request, "new_family.html", context=context)
-    
+
+
+def list_family(request):
+    familys = Family.objects.all()
+    context = {
+        "familys":familys
+    }
+    return render(request, "list_family.html", context=context)
