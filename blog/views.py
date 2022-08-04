3da064356617c8e82b589818205f834eb197e490
alejo ruiz <alejoeliasruiz041@gmail.com>
2022-08-04 17:02:00 -0500

    mejoras

--- blog/views.py
+++ blog/views.py
@@ -5,18 +5,13 @@ from blog.models import Family
 
 def create_family(request):
     new_family = Family.objects.create(name = "Eduardo", years = 55, last_name = "Simpson")
-    
     new_family2 = Family.objects.create(name = "Laura", years = 56, last_name = "Simpson")
-    
     new_family3 = Family.objects.create(name = "Emir", years = 21, last_name = "Simpson")
     
     context = {
         "new_family" :new_family,
-
         "new_family2" :new_family2,
-
         "new_family3" :new_family3
-
     }
     return render(request, "new_family.html", context=context)
 
